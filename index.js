ee8c2c1b12068da04d6bb3b10395954dc80fe117
Lydia Tan <lydia.sydia@gmail.com>
2018-07-11 17:08:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ var excitedWelcomeMessage = "WELCOME TO KLOSSYBOOK, " + upperCaseCurrentUser + "
 const firstInitial = currentUser[0];
 const restOfName = currentUser.slice(1);
 
-var shortGreeting = firstInitial + 
+var shortGreeting = "Welcome, " + firstInitial + restOfName;
